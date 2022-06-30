81fd77f98bd6cf0b2be1aa06bdfe154d7a40d4dc
geek-Shayan <sadi06888@gmail.com>
2022-06-30 12:32:07 +0600

    protected routes done + all CRUD operations perfect

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
     {
         $fields = $request->validate([
             'name' => 'required|string',
-            'email' => 'required|string',
+            'email' => 'required|string|unique:users,email',
             'password' => 'required|string|confirmed',
         ]);
 
@@ -25,7 +25,55 @@ class AuthController extends Controller
             'password' => bcrypt($fields['password']),
         ]);
 
-        // $token = $user->
         // token create
+        $token = $user->createToken('myapptoken')->plainTextToken;
+    
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        // check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // check password
+        if(!$user ||!Hash::check($fields['password'], $user->password))
+        {
+            return response([
+                'messege' => 'bad credentials !'
+            ], 401);
+        }
+
+
+        // token create
+        $token = $user->createToken('myapptoken')->plainTextToken;
+    
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'messege' => 'logged out'
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,8 +17,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//////////// public routes
+//////////// basic defined crud default public routes
 // Route::resource('/products', ProductController::class); // all crud bassed defaullts
+
+
+//////////// public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
 Route::get('/products', [ProductController::class, 'index'] );
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
@@ -27,7 +35,7 @@ Route::group(['middleware'=>['auth:sanctum']], function () {
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-    
+    Route::post('/logout', [AuthController::class, 'logout']);
 
 });
 
